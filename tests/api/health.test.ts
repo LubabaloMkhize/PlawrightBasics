866953e6a9b60e0a8d63e212f8e78042bdfb259c
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2026-05-29 17:31:34 +0200

    Added api tests.

---
+++ tests/api/health.test.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Health API should return 200', async ({ request }) => {
+
+  const response = await request.get(
+    'https://ndosiautomation.co.za/APIDEV/health'
+  );
+
+  // Verify status code
+  expect(response.status()).toBe(200);
+
+  // Print response body
+  const responseBody = await response.text();
+  console.log(responseBody);
+
+});
+
+test('Login API should return 200', async ({ request }) => {
+
+  const response = await request.get(
+    'https://ndosiautomation.co.za/APIDEV/login'
+  );
+
+  // Verify status code
+  expect(response.status()).toBe(200);
+
+  // Print response body
+  const responseBody = await response.text();
+  console.log(responseBody);
+
+});
