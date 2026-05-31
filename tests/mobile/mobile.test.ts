b6c8ca6f90c89c0fabd6c232be4f963a2f4a43f3
LubabaloMkhize <lubabalo.mkhize@gmail.com>
2026-05-31 22:42:36 +0200

    Added Mobile test.

---
+++ tests/mobile/mobile.test.ts
@@ -0,0 +1,20 @@
+import {test,devices,expect} from '@playwright/test';
+
+  test.use({
+    ...devices['iPhone 14 Pro'],
+    
+  });
+
+  test('mobile web test', async ({ page }) => {
+    await page.goto('*/');
+
+    await page.locator('.nav-burger').click();
+
+    await page.locator('button:has-text("Login / Sign Up")').click();
+
+    await expect(page.locator('#login-heading')).toBeVisible();
+
+    console.log(devices);
+
+    await page.waitForTimeout(5000);
+  });
