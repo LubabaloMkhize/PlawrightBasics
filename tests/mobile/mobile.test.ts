3f2fda5eb6e52a8c1f0e6aac3c1ad065ac1720b0
lubabalo.mkhize <Lubabalo.Mkhize@gmail.com>
2026-06-03 14:15:48 +0200

    I have added a mobile login test.

--- tests/mobile/mobile.test.ts
+++ tests/mobile/mobile.test.ts
@@ -2,7 +2,7 @@ import {test,devices,expect} from '@playwright/test';
 
   test.use({...devices['iPhone 14 Pro'],});
 
-  test('mobile web test', async ({ page }) => {
+  test('verify mobile web login page heading', async ({ page }) => {
     await page.goto('*/');
 
     await page.locator('.nav-burger').click();
@@ -11,7 +11,27 @@ import {test,devices,expect} from '@playwright/test';
 
     await expect(page.locator('#login-heading')).toBeVisible();
 
-    console.log(devices);
+    //console.log(devices);
+
+    await page.waitForTimeout(5000);
+  });
+
+  test('login using mobile web', async ({ page }) => {
+    await page.goto('*/');
+
+    await page.locator('.nav-burger').click();
+
+    await page.locator('button:has-text("Login / Sign Up")').click();
+
+    await page.locator('#login-email').fill('Tatalo.Mkhize@example.com');
+
+    await page.locator('#login-password').fill('England@123456');
+
+    await page.locator('#login-submit').click();
+
+    
+
+    //console.log(devices);
 
     await page.waitForTimeout(5000);
   });
